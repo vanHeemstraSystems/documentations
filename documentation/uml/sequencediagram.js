8d4c2266e9150a99f358a15d40a22033e7104132
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-05-26 11:33:23 +0100

    Modified

--- documentation/uml/sequencediagram.js
+++ documentation/uml/sequencediagram.js
@@ -9,17 +9,18 @@ var self = this;
  * @return {SequenceDiagram}
  */
 function SequenceDiagram() {
-  self._foo = 'bar'; // placeholder
-  self._proxies = {}; 
+  self._filename = ''; // placeholder
+  self._filepath = ''; // placeholder
+  self._proxies = {};
 }
 
-SequenceDiagram.prototype.append = function(fileName) {
-  // Note: fileName includes path; e.g.	'path/to/file'
+SequenceDiagram.prototype.append = function() {
   // temporarily we do all the file creation and appending in here
   // move most of this logic out to its best location. e.g. at documentation level
   var fs = self._proxies.proxy().libraries().library().fs(); // WAS require('fs');
-  //var fileName = fileName;
-  var stream = fs.createWriteStream(fileName);
+  var path = self._proxies.proxy().libraries().library().path();
+  var filePathAndName = path.join(self._filepath,self._filename);
+  var stream = fs.createWriteStream(filePathAndName);
   stream.once('open', function(fd) {
     var html = self.buildHtml();
 
@@ -35,6 +36,22 @@ SequenceDiagram.prototype.buildHtml = function() {
   return '<!DOCTYPE html>'
        + '<html><header>' + header + '</header><body>' + body + '</body></html>';
 };
+
+SequenceDiagram.prototype.filename = function() {
+  return self._filename;
+}
+
+SequenceDiagram.prototype.setfilename = function(fnOrValue) {
+  self._filename = fnOrValue;
+}
+
+SequenceDiagram.prototype.filepath = function() {
+  return self._filepath;
+}
+
+SequenceDiagram.prototype.setfilepath = function(fnOrValue) {
+  self._filepath = fnOrValue;
+}
 
 SequenceDiagram.prototype.proxies = function() {
   return self._proxies;
