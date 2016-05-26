e58315121da51297d19915e1898b1b707c40867b
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-05-26 09:37:14 +0100

    Modified

--- documentation/uml/sequencediagram.js
+++ documentation/uml/sequencediagram.js
@@ -10,10 +10,38 @@ var self = this;
  */
 function SequenceDiagram() {
   self._foo = 'bar'; // placeholder
+  self._proxies = {}; 
 }
 
-SequenceDiagram.prototype.append = function() {
-  // do something
+SequenceDiagram.prototype.append = function(fileName) {
+  // Note: fileName includes path; e.g.	'path/to/file'
+  // temporarily we do all the file creation and appending in here
+  // move most of this logic out to its best location. e.g. at documentation level
+  var fs = self._proxies.proxy().libraries().library().fs(); // WAS require('fs');
+  //var fileName = fileName;
+  var stream = fs.createWriteStream(fileName);
+  stream.once('open', function(fd) {
+    var html = self.buildHtml();
+
+    stream.end(html);
+  });
+}
+
+SequenceDiagram.prototype.buildHtml = function() {
+  var header = '';
+  var body = '';
+  // concatenate header string
+  // concatenate body string
+  return '<!DOCTYPE html>'
+       + '<html><header>' + header + '</header><body>' + body + '</body></html>';
+};
+
+SequenceDiagram.prototype.proxies = function() {
+  return self._proxies;
+}
+
+SequenceDiagram.prototype.setproxies = function(fnOrValue) {
+  self._proxies = fnOrValue;
 }
 
 module.exports = SequenceDiagram;
