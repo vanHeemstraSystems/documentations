9a8e12b95a9352963f5d12e14663d754652932c0
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-05-29 16:14:05 +0100

    Modified

--- documentation/uml/sequencediagram.js
+++ documentation/uml/sequencediagram.js
@@ -21,6 +21,7 @@ SequenceDiagram.prototype.append = function() {
   //var fs = self._proxies.proxy().libraries().library().fs(); // DOESN'T WORK!!
   var path = self._proxies.proxy().libraries().library().path();
   var filePathAndName = path.join(self._filepath, self._filename);
+  var _buildHtml = this.buildHtml();
   this.ensureExists(self._filepath, 0744, function(err) {
     if (err) { // handle folder creation error
       console.log('documentations documentation uml sequencediagram - error', err)
@@ -29,7 +30,7 @@ SequenceDiagram.prototype.append = function() {
       var fs = require('fs');
       var stream = fs.createWriteStream(filePathAndName);
       stream.once('open', function(fd) {
-        var html = self.buildHtml();
+        var html = _buildHtml;
 
         stream.end(html);
       });    	
@@ -38,7 +39,7 @@ SequenceDiagram.prototype.append = function() {
 }
 
 SequenceDiagram.prototype.buildHtml = function() {
-  console.log('documentations documentation uml sequencediagram - buitlHTML() called')	
+  console.log('documentations documentation uml sequencediagram - buildHtml() called')
   var header = '';
   var body = '';
   // concatenate header string
@@ -47,22 +48,35 @@ SequenceDiagram.prototype.buildHtml = function() {
        + '<html><header>' + header + '</header><body>' + body + '</body></html>';
 };
 
-SequenceDiagram.prototype.ensureExists = function(path, mask, cb) {
+// This function is documented here:
+// http://lmws.net/making-directory-along-with-missing-parents-in-node-js
+SequenceDiagram.prototype.ensureExists = function(path, mask, callback) {
+  console.log('documentations documentation uml sequencediagram - ensureExists(path, mask, callback) called')
+  console.log('documentations documentation uml sequencediagram - ensureExists: path: ', path)
+  console.log('documentations documentation uml sequencediagram - ensureExists: mask: ', mask)
   if (typeof mask == 'function') { // allow the `mask` parameter to be optional
-    cb = mask;
+    callback = mask;
     mask = 0777; // Read and Write permissions
   }
   var fs = require('fs');
-  fs.mkdir(path, mask, function(err) {
-    if (err) {
-      if (err.code == 'EEXIST') { 
-      	cb(null);
+  fs.mkdir(path, mask, function(error) {
+    if (error) {
+      if (error.code == 'EEXIST') { 
+      	callback(null);
       } // ignore the error if the folder already exists
       else {
-        cb(err);
+        //When it fail in this way, do the custom steps
+        if (error && error.errno === 34) {
+          //Create all the parents recursively
+          fs.mkdirParent(path.dirname(dirPath), mode, callback);
+          //And then the directory
+          fs.mkdirParent(dirPath, mode, callback);
+        }
+        //Manually run the callback since we used our own callback to do all these
+        callback && callback(error);
       } // something else went wrong
     } else { 
-      cb(null); 
+      callback(null);
     } // successfully created folder
   });
 };
